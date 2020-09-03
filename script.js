dea3c5a597c74ba5a961512f1cfb39a619de04f9
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-03 09:40:35 +0200

    skapar namn, array och element till sidan

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//Min externa script-fil
-function myName() {
-  document.getElementById("").textContent = "Alma Härlin";
-}
-console.log(myName());
+//skapa alla element som behöver vara på sidan
+let nameDiv = document.createElement("div");
+let arrayDiv = document.createElement("div");
+let nameParagraf = document.createElement("p");
+//skapa sträng variable med mitt namn
+let myName = document.write("Alma");
+//skapa och lägga till element i array
+let myArray = ["Programmera", "Måla", "Ju-jutsu", "Hjälpa andra"];
